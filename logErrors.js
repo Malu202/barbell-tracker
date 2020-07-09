2ebd65fe407b0339ddb19665aeaf64e593a69f3e
Malu202 <poehlmann.martin@gmail.com>
2020-07-09 03:12:20 +0200

    change error displaying

--- logErrors.js
+++ logErrors.js
@@ -1,24 +1,25 @@
-window.onerror = function (error) {
-    displayError(error)
-    return false;
+// window.onerror = function (error) {
+//     displayError(error)
+//     return false;
 
-}
+// }
 window.addEventListener("error", function (error) {
     displayError(error)
     return false;
 
 })
-window.addEventListener('unhandledrejection', function (event) {
-    displayError(event)
-    return false;
+// window.addEventListener('unhandledrejection', function (event) {
+//     displayError(event)
+//     return false;
 
-})
+// })
 
 function displayError(error) {
-    if (typeof error === 'string' || error instanceof String) {
-        alert(error)
-    } else {
-        alert("Error in: " + error.filename + '\n' + error.stack)
-    }
+    // if (typeof error === 'string' || error instanceof String) {
+    //     alert(error)
+    // } else {
+    //     alert("Error in: " + error.filename + '\n' + error.stack)
+    // }
+    alert(error.stack);
     throw error;
 }
